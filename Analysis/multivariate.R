55aecb0273b43e0cc389679ef30d18a58277aa19
brigittewylee <brigitte.leewy@gmail.com>
2025-07-07 22:34:49 -0400

    adds multivar analysis

--- Analysis/multivariate.R
+++ Analysis/multivariate.R
@@ -5,31 +5,21 @@
 
 library(pROC)
 
-# top_2018 <- read.csv("CleanedData(Num)/clean_2018n", header = TRUE)
-# top_2019 <- read.csv("CleanedData(Num)/clean_2019n", header = TRUE)
-# top_2020 <- read.csv("CleanedData(Num)/clean_2020n", header = TRUE)
-# top_2021 <- read.csv("CleanedData(Num)/clean_2021n", header = TRUE)
-# top_2022 <- read.csv("CleanedData(Num)/clean_2022n", header = TRUE)
-# top_2023 <- read.csv("CleanedData(Num)/clean_2023n", header = TRUE)
-
-top_2018 <- read.csv("RaceData/clean_2018", header = TRUE)
-top_2019 <- read.csv("RaceData/clean_2019", header = TRUE)
-top_2020 <- read.csv("RaceData/clean_2020", header = TRUE)
-top_2021 <- read.csv("RaceData/clean_2021", header = TRUE)
-top_2022 <- read.csv("RaceData/clean_2022", header = TRUE)
-top_2023 <- read.csv("RaceData/clean_2023", header = TRUE)
-
-
-
+top_2018 <- read.csv("CleanedData(Num)/clean_2018n", header = TRUE)
+top_2019 <- read.csv("CleanedData(Num)/clean_2019n", header = TRUE)
+top_2020 <- read.csv("CleanedData(Num)/clean_2020n", header = TRUE)
+top_2021 <- read.csv("CleanedData(Num)/clean_2021n", header = TRUE)
+top_2022 <- read.csv("CleanedData(Num)/clean_2022n", header = TRUE)
+top_2023 <- read.csv("CleanedData(Num)/clean_2023n", header = TRUE)
 
 fit_glm <- function(df, year = "Unknown") {
-  factored_df <- within (df, {BULLIED = as.factor(BULLIED)
+  factored_df <- within(df, {BULLIED = as.factor(BULLIED)
                               MAKEFRIEND = as.factor(MAKEFRIEND)})
   glmodel <- glm(BULLIED ~ MAKEFRIEND + SEX + INCOME + ADHD_MEDICATION +
                    ADHD_BT,
                  family = binomial, data = factored_df)
 
-coefs <- summary(glmodel)$coefficients
+  coefs <- summary(glmodel)$coefficients
   beta_val <- coefs[-1, 1]
   std_errors <- coefs[-1, 2]
 
@@ -51,7 +41,6 @@ coefs <- summary(glmodel)$coefficients
   )
 
   prediction <- predict(glmodel, factored_df, type = "response")
-
   roc_object <- roc(factored_df$BULLIED, prediction)
   auc_value <- auc(roc_object)
 
@@ -69,12 +58,14 @@ coefs <- summary(glmodel)$coefficients
   plot(roc_object, main = paste("ROC Curve", year))
   print(auc_value)
 
+  print(gplot)
+
   cat("===================================================\n\n")
 }
 
-fit_glm(top_2018, 2018)
-fit_glm(top_2019, 2019)
-fit_glm(top_2020, 2020)
+# fit_glm(top_2018, 2018)
+# fit_glm(top_2019, 2019)
+# fit_glm(top_2020, 2020)
 fit_glm(top_2021, 2021)
-fit_glm(top_2022, 2022)
-fit_glm(top_2023, 2023)
+# fit_glm(top_2022, 2022)
+# fit_glm(top_2023, 2023)
